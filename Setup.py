c3d10e4905160385de83ac69a63accf13c3de55e
sigmachipmunk <skibidibro123556@gmail.com>
2025-01-11 21:44:19 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -9,14 +9,14 @@ try:
         os.system("cls")
         os.system("python -m pip install --upgrade pip")
         os.system("python -m pip install -r requirements.txt")
-        os.system("python RedTiger.py")
+        os.system("python Azora.py")
 
     elif sys.platform.startswith("linux"):
         print("Installing the python modules required for the Azora Multi-Tool:")
         os.system("clear")
         os.system("python3 -m pip3 install --upgrade pip")
         os.system("python3 -m pip3 install -r requirements.txt")
-        os.system("python3 RedTiger.py")
+        os.system("python3 Azora.py")
 
 except Exception as e:
     print(e)
